9487727949ade424185f731e26ce3662c270839f
user <mazay3521@mail.ru>
2023-10-02 12:14:40 +0300

    123

---
+++ dima.java
@@ -0,0 +1,19 @@
+public class Answer {
+    public static void main(String[] args) {
+        int num1 = 1;
+        int num2 = 0;
+        int result = num1 + num2;
+
+        String equation = num1 + " + " + num2 + " = " + result;
+        System.out.println(equation);
+    }
+}
+
+
+//вот пример как оно реализовывается на js 
+// const num1 = 1;
+//const num2 = 0;
+//const result = num1 + num2;
+//const equation = `${num1} + ${num2} = ${result}`;
+
+//console.log(equation);
